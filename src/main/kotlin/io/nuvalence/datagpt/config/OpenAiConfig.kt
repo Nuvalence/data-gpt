8ee3127cc50a0bc2b8ed51c1e08777947f1bb863
Jay Reimers <jreimers@nuvalence.io>
2023-02-24 12:57:53 -0500

    chore: remove Open AI read timeout

--- src/main/kotlin/io/nuvalence/datagpt/config/OpenAiConfig.kt
+++ src/main/kotlin/io/nuvalence/datagpt/config/OpenAiConfig.kt
@@ -4,13 +4,14 @@ import com.theokanning.openai.service.OpenAiService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.time.Duration
 
 @Configuration
 class OpenAiConfig {
 
     @Bean
     fun openAiService(@Value("\${openai.api-key}") apiKey: String): OpenAiService {
-        return OpenAiService(apiKey)
+        return OpenAiService(apiKey, Duration.ZERO)
     }
 
 }
